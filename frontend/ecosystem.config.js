5114676123967faf04869606c0dfa3a974cc0626
Yura Dudin <dudinyv@yandex.ru>
2023-07-09 22:57:24 +0500

    add pm2 frontend

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -6,11 +6,6 @@ const {
 } = process.env;
 
 module.exports = {
-  apps: [{
-    name: "mesto",
-    script: "dist/app.js"
-  }],
-
   // Настройка деплоя
   deploy: {
     production: {
@@ -20,7 +15,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build',
     },
   },
 };
